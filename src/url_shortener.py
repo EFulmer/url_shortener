c9c035b1b8b87318614646a6ca0a80b9e118952a
Eric Fulmer <EFulmer@gmail.com>
2014-01-04 16:16:45 -0500

    first words!!!

--- src/url_shortener.py
+++ src/url_shortener.py
@@ -2,14 +2,21 @@ import sqlite3
 
 from flask import Flask
 
+import config
+
+
 app = Flask(__name__)
 
 
+@app.route('/')
+def show_mainpage():
+    return 'hello world'
 
 def main():
     # TODO:
     # check for DB's existence and if it doesn't exist, initialize it
     # using schema.sql; part this out into its own fn obviously.
+    app.config.from_object(config)
     app.run()
 
 
