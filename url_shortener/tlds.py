22fad67b9ba773b00cfa95188d5e00908e84e93f
Eric Fulmer <EFulmer@gmail.com>
2014-01-19 02:56:25 -0500

    bugfix: empty string no longer counts as a valid tld

--- url_shortener/tlds.py
+++ url_shortener/tlds.py
@@ -7,22 +7,30 @@ TLD_FILE = 'tlds.txt'
 
 
 def get_tlds(tld_site=TLD_URL):
+    """
+    Read in Mozilla's TLD list, parse the TLDs out of it, and return
+    all of the TLDs in it.
+
+    A URL to an alternative TLD list can be provided. If it is, all 
+    the lines in it should either contain TLDs (and nothing else) or 
+    start with whitespace or a forward-slash ('/')
+    """
     text = requests.get(tld_site).text
-    tlds = [ line for line in text.split('\n') 
-             if not line.startswith('/') or line == '' or line.startswith(' ') ]
-    return set(tlds)
+    tlds = { line for line in text.split('\n')[1:]
+             if not line.startswith('/')  or line.startswith(' ') }
+    tlds = tlds - {''}
+    return frozenset(tlds)
 
 
 tlds = get_tlds(TLD_URL)
 
 
 def write_tlds_to_file(tlds=tlds, tld_file=TLD_FILE):
+    """Write all of the TLDs to a file."""
     with open(tld_file, 'w') as f:
         f.write('\n'.join(tlds))
 
 
 def has_valid_tld(url, tlds=tlds):
-    # TODO: better way of making sure that has_valid_tld('') doesn't 
-    # returh True
     url_tld = extract(url).suffix
-    return url_tld in tlds and url_tld != ''
+    return url_tld in tlds
