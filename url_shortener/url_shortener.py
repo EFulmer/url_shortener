92830449bccac35fb15347b5d3bf2c773953b4df
Eric Fulmer <EFulmer@gmail.com>
2014-01-23 21:01:27 -0500

    now recognizes uri scheme

--- url_shortener/url_shortener.py
+++ url_shortener/url_shortener.py
@@ -1,6 +1,7 @@
 from contextlib import closing
 import os
 import sqlite3
+from urllib.parse import urlparse
 
 from flask import Flask
 from flask import request
@@ -27,7 +28,7 @@ def connect_db():
 def init_db():
     """Initialize a new database."""
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('./schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
@@ -71,18 +72,22 @@ def add_url():
         return redirect(url_for('show_mainpage'))
 
     try:
+        # assume http if no protocol/scheme given
+        if urlparse(url).scheme == '':
+            url = 'http://' + url
+
         cr = g.db.cursor()
         cr.execute('INSERT INTO Link (longurl) VALUES (?);',
-                [request.form['url']])
+                [url])
         cr.execute('INSERT INTO Redirect (longurl, count) VALUES (?, 0);',
-                [request.form['url']])
+                [url])
         res = cr.execute('SELECT id FROM Link WHERE longurl = (?);',
-                [request.form['url']])
+                [url])
 
         short_url = res.fetchone()[0]
         g.db.commit()
         cr.close()
-    except Exception:
+    except Exception as e:
         # TODO log error msg and send to me.
         flash("We're sorry, but an error occurred.")
     else:
